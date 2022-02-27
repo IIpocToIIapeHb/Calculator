0cc75b1680d249a6d99a3686db17a26139e96c01
KpacuBbIuIIapeHb <kostiarudoy2303@gmail.com>
2022-02-27 13:11:10 +0300

    fourth

--- src/by/home/java_fundamentals/calc/Main.java
+++ src/by/home/java_fundamentals/calc/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		
+		
+		
 		String expression = "2+(2*2)";
 		Calculator calculator = new Calculator();
 		double result = calculator.calculate(expression);
