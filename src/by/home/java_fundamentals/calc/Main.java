1dab9458b536a99c7d635ff326b033931db81935
KpacuBbIuIIapeHb <kostiarudoy2303@gmail.com>
2022-02-27 11:55:28 +0300

    second

--- src/by/home/java_fundamentals/calc/Main.java
+++ src/by/home/java_fundamentals/calc/Main.java
@@ -4,10 +4,12 @@ package by.home.java_fundamentals.calc;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		String expression = "2+(2*2)";
 		Calculator calculator = new Calculator();
 		double result = calculator.calculate(expression);
 		System.out.println(result);
+		
 	}
 
 }
